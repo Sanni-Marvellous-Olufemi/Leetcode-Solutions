a095c37419baf9fae17aeaf30a2d8eee4c74c34c
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2025-10-06 00:42:10 -0500

    31 ms (67.84%) 19.4 MB (44.54%) 10-06-2025 0417-pacific-atlantic-water-flow Medium .py

--- LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -11,12 +11,11 @@ class Solution:
                 return 
 
             ocean.add(k)
-            opt1 = walk(r, c+1, heights[r][c], ocean)
-            opt2 = walk(r, c-1, heights[r][c], ocean)
-            opt3 = walk(r-1, c, heights[r][c], ocean)
-            opt4 = walk(r+1, c, heights[r][c], ocean)
+            walk(r, c+1, heights[r][c], ocean)
+            walk(r, c-1, heights[r][c], ocean)
+            walk(r-1, c, heights[r][c], ocean)
+            walk(r+1, c, heights[r][c], ocean)
 
-            return 
 
         for r in range(row):
             walk(r, 0, 0, pac)
@@ -28,9 +27,16 @@ class Solution:
                     
         for i in pac:
             if i in atl:
-                j, k = i
-                ans.append([j,k])
+                ans.append(list(i))
 
         return ans
 
-            
+
+"""
+-You just need to recursively find all cells that can flow to each ocean then return the intersection
+-Iterate through the island's borders, calling dfs on each border cell
+-The parameters should be the coordinates (r,c), the previous cell and a set which tells the ocean borders cells you are currently iterating through. Call dfs with set pac (pacific) when iterating through the island's left and top edges, then use set atl (atlantic) when iterating through the  island's right and bottom edges.
+-The dfs would automatically traverse the cells neighbors so add the current cell to the ocean set you passed into the function
+-In the recursive function, if previous cell is greater than current cell or index is invalid or the current cell is already in the ocean set, return, Else traverse all north, south, east, and west neighbors
+-If you called dfs on all border cells, the both ocean sets would have all the indices that can flow to them so return a list of both sets intersection (Indices that can flow to both oceans)
+"""    
