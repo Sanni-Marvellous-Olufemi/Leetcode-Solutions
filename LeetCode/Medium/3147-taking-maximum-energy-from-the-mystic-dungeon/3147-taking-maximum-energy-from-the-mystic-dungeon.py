4089de562be3135402eef69a00b7234b31354a55
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2025-10-10 18:17:39 -0500

    1194 ms (28.92%) 31.4 MB (25.49%) 10-10-2025 3147-taking-maximum-energy-from-the-mystic-dungeon Medium .py

--- LeetCode/Medium/3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.py
+++ LeetCode/Medium/3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.py
@@ -4,10 +4,16 @@ class Solution:
 
         for i in range(len(energy)):
             if i - k >= 0:
-                energy[i] = max(energy[i] + energy[i-k], energy[i])
+                energy[i] = max(energy[i] + energy[i - k], energy[i])
 
-        for i in range(len(energy)-k, len(energy)):
+        for i in range(len(energy) - k, len(energy)):
             ans = max(ans, energy[i])
 
         return ans
-        
+
+
+"""
+-Use prefix sum to calculate the sum of all indices. Calculate it so that each index has the sum of all i % k indices before it.
+-But don't use a blind prefix sum. Use a kadanes prefix sum so that each index has the max of all i % k indices before it. So if the current index is greater than the i % k sum, it greedily picks the current index instead of adding the current index value to the i % k sum
+-When you're done creating the kadane's prefix sum array, iterate through the last k elements and return the max 
+"""
