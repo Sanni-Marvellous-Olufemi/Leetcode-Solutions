157e03d44c1f247d3581ac6b62749ae9fbb70edf
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2025-10-22 16:56:45 -0500

    213 ms (73.08%) 29.4 MB (78.53%) 10-22-2025 2779-maximum-beauty-of-an-array-after-applying-operation Medium .py

--- LeetCode/Medium/2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ LeetCode/Medium/2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,7 +1,7 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         nums.sort()
-        dif, left, ans = [0] * len(nums), 0, 0
+        left, ans = 0, 0
 
         for right in range(1, len(nums)):
             while nums[right] - nums[left] > (k * 2):
