02cad38355505f2a04cbd506a3ba8ecf33942ccd
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2025-10-24 01:50:10 -0500

    7 ms (52.63%) 19.3 MB (7.97%) 10-24-2025 0784-letter-case-permutation Medium .py

---
+++ LeetCode/Medium/0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        
+        res = set()
+        def recurse(i, path):
+            if i == len(s):
+                res.add(path)
+                return    
+            if s[i].isalpha():
+                recurse(i + 1, path + s[i].upper())
+                recurse(i + 1, path + s[i].lower())
+            else:
+                recurse(i+1, path + s[i])
+        recurse(0, "")
+        return list(res)
+        
