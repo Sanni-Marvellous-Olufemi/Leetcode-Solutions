13d70d997a6920652ab36fab9160a49c3f0d808a
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2025-10-27 18:46:45 -0500

    0 ms (100%) 18 MB (27.29%) 10-27-2025 3442-maximum-difference-between-even-and-odd-frequency-i Easy .py

--- LeetCode/Easy/3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.py
+++ LeetCode/Easy/3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.py
@@ -1,13 +1,13 @@
 class Solution:
     def maxDifference(self, s: str) -> int:
-        arr = sorted(Counter(s).values())
-        
-        start, end = 0, len(arr)-1
+        arr = Counter(s).values()
+        start, end = float("inf"), 0
 
-        while start < len(arr) and arr[start] % 2 == 1:
-            start += 1
+        for i in arr:
+            if i % 2 == 0 and i < start:
+                start = i
 
-        while end >= 0 and arr[end] % 2 == 0:
-            end -= 1
+            if i % 2 == 1 and i > end:
+                end = i
 
-        return arr[end] - arr[start]
+        return end - start
