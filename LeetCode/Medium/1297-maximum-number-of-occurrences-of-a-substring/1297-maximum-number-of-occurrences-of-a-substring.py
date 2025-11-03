c8ad026f7065dc30c5b485a1999d3eadfd8eb2e1
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2025-11-03 00:47:27 -0600

    93 ms (66.51%) 19.5 MB (42.26%) 11-03-2025 1297-maximum-number-of-occurrences-of-a-substring Medium .py

--- LeetCode/Medium/1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.py
+++ LeetCode/Medium/1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.py
@@ -1,18 +1,12 @@
 class Solution:
     def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
-        hashmap = defaultdict(int)
-        left, y = 0, False
+        hashmap, left = defaultdict(int), 0
 
         for right in range(len(s)):
             sets = set(s[left:right+1])
 
             if right - left + 1 == minSize:
-                if len(sets) <= maxLetters:
-                    hashmap[s[left:right+1]] += 1
-                    
+                hashmap[s[left:right+1]] += 1 if len(sets) <= maxLetters else 0
                 left += 1
 
-        
         return max(hashmap.values()) if hashmap else 0
-
-
