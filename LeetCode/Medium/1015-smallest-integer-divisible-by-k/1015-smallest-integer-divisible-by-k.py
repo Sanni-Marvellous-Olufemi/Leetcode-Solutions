32931d022ec8d704d6ed13015cb12b36095ca35b
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2025-11-25 17:05:58 -0600

    973 ms (19.41%) 21.7 MB (12.45%) 11-25-2025 1015-smallest-integer-divisible-by-k Medium .py

--- LeetCode/Medium/1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
+++ LeetCode/Medium/1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.py
@@ -3,12 +3,18 @@ class Solution:
         if k % 2 == 0:
             return -1
 
-        start = "1"
+        if k == 1:
+            return k
 
-        for i in range(6):
-            if int(start) % k == 0:
-                return len(start)
+        sets = set()
+        rem, curr = 1 % k, 1
 
-            start += "1"
+        while rem not in sets:
+            sets.add(rem)
+            curr = (curr * 10) + 1
+            rem = curr % k
+    
+            if rem == 0:
+                return int(math.log10(curr)) + 1
 
-        return len(start) if int(start) % k == 0 else -1
+        return -1
