dffb4be87661526cade17193ae4034caeda2f9f3
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2025-12-13 21:22:01 -0600

    N/A (0%) N/A (0%) 12-13-2025 0091-decode-ways Medium .py

---
+++ LeetCode/Medium/0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {}
+
+        if s[0] == "0" or "00" in s:
+            return 0
+
+        def walk(i):
+            if i < len(s) and s[i] == "0":
+                return 0
+
+            if i >= len(s)-1:
+                return 1
+            
+            if i in memo:
+                return memo[i]
+
+            opt1 = 0
+            if (s[i] == "2") and (i < len(s)-1) and ("1" <= s[i+1] <= "6"):
+                opt1 = walk(i+2)
+
+            if (s[i] == "1"):
+                opt1 = walk(i+2)
+
+            opt2 = walk(i+1)
+            memo[i] = opt1 + opt2
+            return memo[i]
+
+        return walk(0)
+
+            
