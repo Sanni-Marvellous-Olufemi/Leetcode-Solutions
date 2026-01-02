6179e026cf8d4c6b80abdc2b277bc3099201e4b8
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-01-02 15:35:18 -0600

    6 ms (42.37%) 61.8 MB (22.03%) 01-02-2026 0961-n-repeated-element-in-size-2n-array Easy .js

--- LeetCode/Easy/0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
+++ LeetCode/Easy/0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 var repeatedNTimes = function(nums) {
-    let hashmap = {};
+    let hashmap = new Map;
 
     for (let i = 0; i < nums.length; i++) {
-        if (!(nums[i] in hashmap)) {
-            hashmap[nums[i]] = 0;
-        }
-        hashmap[nums[i]]++;
+        hashmap.set(nums[i], (hashmap.get(nums[i]) ?? 0) + 1);
         
-        if (hashmap[nums[i]] == (nums.length/2)) {
+        if (hashmap.get(nums[i]) == (nums.length/2)) {
             return nums[i]
         }
     }
