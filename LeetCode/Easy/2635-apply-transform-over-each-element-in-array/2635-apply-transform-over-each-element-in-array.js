936d9fc765d570132321239cbe1d496cca22c70d
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-01-04 16:35:50 -0600

    41 ms (77.28%) 55 MB (7.42%) 01-04-2026 2635-apply-transform-over-each-element-in-array Easy .js

---
+++ LeetCode/Easy/2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let ans = new Array;
+
+    for (let i = 0; i < arr.length; i++) {
+        ans.push(fn(arr[i], i));
+    };
+
+    return ans;
+};
