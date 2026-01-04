e7b0c2ba2737d9160f167ed78cb05c7f057a890c
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-01-04 16:24:53 -0600

    N/A (0%) N/A (0%) 01-04-2026 1390-four-divisors Medium .py

---
+++ LeetCode/Medium/1390-four-divisors/1390-four-divisors.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        ans = 0
+
+        for num in nums:
+            count, curr, sqrt = 0, 0, int(num ** (1/2))
+
+            for i in range(1, sqrt):
+                if num % i == 0:
+                    count += 2 if (num/i) > sqrt else 1
+                    curr += i
+                    curr += num / i if (num/i) > sqrt else 0
+
+            ans += (curr) if count == 4 else 0
+
+        return int(ans)
