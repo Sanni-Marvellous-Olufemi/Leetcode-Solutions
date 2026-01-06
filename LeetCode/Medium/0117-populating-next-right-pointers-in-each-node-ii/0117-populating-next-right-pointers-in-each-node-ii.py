d62be0b21e0c2fc7a2eae1e424e7c683e85effc6
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-01-06 14:03:15 -0600

    59 ms (5.34%) 20.5 MB (12.21%) 01-06-2026 0117-populating-next-right-pointers-in-each-node-ii Medium .py

---
+++ LeetCode/Medium/0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,33 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        queue = deque()
+        queue.append(root)
+
+        while queue and root:
+            n = len(queue)
+            
+            for i in range(len(queue)):
+                node = queue.popleft()
+
+                if queue and i < n-1:
+                    node.next = queue[0]
+                else:
+                    node.next = None
+
+                if node.left:
+                    queue.append(node.left)
+                
+                if node.right:
+                    queue.append(node.right)
+
+        return root
