adf5992b77f4d6469741f67d4d1b5500733856f0
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-01-25 15:17:17 -0600

    N/A (0%) N/A (0%) 01-25-2026 1984-minimum-difference-between-highest-and-lowest-of-k-scores Easy .js

---
+++ LeetCode/Easy/1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minimumDifference = function(nums, k) {
+    nums.sort();
+    let ans = Infinity;
+
+    for (let i = 0, j = k-1; j < nums.length; i++, j++){
+        ans = Math.min(ans, nums[j] - nums[i]);
+    }
+
+    return ans === Infinity? 0 : ans;
+};
