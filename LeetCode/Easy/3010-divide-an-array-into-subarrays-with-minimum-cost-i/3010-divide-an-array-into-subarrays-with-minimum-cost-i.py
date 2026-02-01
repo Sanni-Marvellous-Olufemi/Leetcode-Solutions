844a3a68033142a25037f7c670cc8b4dc160a2e0
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-02-01 17:10:39 -0600

    0 ms (100%) 19.5 MB (15.54%) 02-01-2026 3010-divide-an-array-into-subarrays-with-minimum-cost-i Easy .py

--- LeetCode/Easy/3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ LeetCode/Easy/3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -4,9 +4,7 @@ class Solution:
         curr1, curr2 = 1, 2
 
         for i in range(3, len(nums)):
-            
             if nums[i] <= nums[curr2] or nums[i] <= nums[curr1]:
-                # print(nums[i], nums[curr2], nums[curr1])
                 if nums[curr2] <= nums[curr1]:
                     curr1 = curr2
                 curr2 = i
