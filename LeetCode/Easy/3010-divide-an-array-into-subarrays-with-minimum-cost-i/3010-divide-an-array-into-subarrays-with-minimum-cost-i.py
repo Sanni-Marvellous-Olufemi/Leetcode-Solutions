19895f01a90b1651a0f9172b7a147b22992d3ee9
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-02-01 17:10:09 -0600

    N/A (0%) N/A (0%) 02-01-2026 3010-divide-an-array-into-subarrays-with-minimum-cost-i Easy .py

---
+++ LeetCode/Easy/3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumCost(self, nums: List[int]) -> int:
+        ans = nums[0]
+        curr1, curr2 = 1, 2
+
+        for i in range(3, len(nums)):
+            if nums[i] <= nums[curr2]:
+                if nums[curr2] <= nums[curr1]:
+                    curr1 = curr2
+                curr2 = i
+
+        return ans + nums[curr1] + nums[curr2]
