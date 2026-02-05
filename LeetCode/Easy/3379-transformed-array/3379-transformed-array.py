de1a4383b8cda91e03278639fb489854c0cac5c8
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-02-05 16:41:44 -0600

    54 ms (85.65%) 19.3 MB (58.23%) 02-05-2026 3379-transformed-array Easy .py

---
+++ LeetCode/Easy/3379-transformed-array/3379-transformed-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        ans = []
+
+        for i in range(len(nums)):
+            ans.append(nums[(i+nums[i]) % len(nums)])
+
+        return ans
