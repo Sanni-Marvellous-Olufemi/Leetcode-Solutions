243a94a295c301d74d64d14065fe6f13a6e664a4
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-02-12 16:57:44 -0600

    2886 ms (7.14%) 63.4 MB (7.14%) 02-12-2026 3713-longest-balanced-substring-i Medium .js

---
+++ LeetCode/Medium/3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestBalanced = function(s) {
+    let ans = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let hashmap = {};
+
+        for (let j = i; j < s.length; j++){
+            hashmap[s[j]] = hashmap[s[j]] ? hashmap[s[j]] + 1 : 1;
+            ans = check(hashmap,hashmap[s[j]]) ? Math.max(ans, j-i+1) : ans
+        }
+    }
+
+    function check(obj, curr) {
+        for (let k of Object.values(obj)) {
+            if (k != curr){
+                return false;
+            };
+        };
+        return true;
+    };
+
+    return ans;
+};
