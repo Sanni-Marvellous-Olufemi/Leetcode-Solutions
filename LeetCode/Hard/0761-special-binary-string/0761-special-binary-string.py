e51eb519c689c91500cbc46c25c1281c7495270e
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-02-20 19:35:18 -0600

    0 ms (100%) 19.5 MB (16.29%) 02-20-2026 0761-special-binary-string Hard .py

--- LeetCode/Hard/0761-special-binary-string/0761-special-binary-string.py
+++ LeetCode/Hard/0761-special-binary-string/0761-special-binary-string.py
@@ -1,19 +1,22 @@
 class Solution:
     def makeLargestSpecial(self, s: str) -> str:
-        i = 0
-        count = 0
-        special_substrings = []
-        
-        for j, char in enumerate(s):
-            if char == '1':
-                count += 1
-            else:
-                count -= 1
-            
-            if count == 0:
-                inner = self.makeLargestSpecial(s[i+1:j])
-                special_substrings.append('1' + inner + '0')
-                i = j + 1
-        
-        special_substrings.sort(reverse=True)
-        return ''.join(special_substrings)
+
+        def walk(i, end):
+            count = 0
+            curr = []
+
+            while i <= end:
+                for j in range(i, end):
+                    count += 1 if s[j] == "1" else -1
+
+                    if count == 0:
+                        curr.append("1" + walk(i+1, j) + "0")
+                        i = j + 1
+                        break
+                else:
+                    i += 1
+                
+            curr.sort(reverse = True)
+            return "".join(curr)
+
+        return walk(0, len(s))
