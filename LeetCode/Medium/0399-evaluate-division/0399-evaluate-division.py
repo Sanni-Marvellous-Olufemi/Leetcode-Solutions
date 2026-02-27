4172a1a33c69938a6df0cc296b0f0fd3967260a7
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-02-27 17:58:12 -0600

    0 ms (100%) 19.5 MB (82.59%) 02-27-2026 0399-evaluate-division Medium .py

--- LeetCode/Medium/0399-evaluate-division/0399-evaluate-division.py
+++ LeetCode/Medium/0399-evaluate-division/0399-evaluate-division.py
@@ -9,15 +9,13 @@ class Solution:
             graph[u].append((v, e))
             graph[v].append((u, 1/e))
 
-        for start, end in queries:
+        def walk(start, end):
             if start not in graph or end not in graph:
-                ans.append(-1.0)
-                continue
+                return -1.0
 
             queue = deque()
             sets = set()
             queue.append((start, 1))
-            y = False
 
             while queue:
                 node, e = queue.popleft()
@@ -27,18 +25,17 @@ class Solution:
                 sets.add(node)
 
                 if node == end:
-                    ans.append(e)
-                    y = True
-                    break
+                    return e
 
                 for child, w in graph[node]:
                     if child in sets:
                         continue
 
                     queue.append((child, e * w))
+            return -1.0
 
-            if not y:
-                ans.append(-1.0)
+        for start, end in queries:
+            ans.append(walk(start, end))
 
         return ans
                     
