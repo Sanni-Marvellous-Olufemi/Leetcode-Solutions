29a776ee19709ab7e2a36684d5cd4c4f3e2e2ae2
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-03-07 18:28:48 -0600

    N/A (0%) N/A (0%) 03-07-2026 1888-minimum-number-of-flips-to-make-the-binary-string-alternating Medium .py

--- LeetCode/Medium/1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ LeetCode/Medium/1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -1,51 +1,24 @@
 class Solution:
     def minFlips(self, s: str) -> int:
-        n = len(s)
-        s = s + s  # Double the string to handle Type-1 rotations
-        
-        # Two target alternating patterns
-        target1 = ""
-        target2 = ""
-        for i in range(len(s)):
-            target1 += "0" if i % 2 == 0 else "1"
-            target2 += "1" if i % 2 == 0 else "0"
-            
-        res = float('inf')
-        diff1, diff2 = 0, 0
-        
-        l = 0
-        for r in range(len(s)):
-            # Add cost for the incoming character
-            if s[r] != target1[r]:
-                diff1 += 1
-            if s[r] != target2[r]:
-                diff2 += 1
-                
-            # If window size > n, remove the cost for the outgoing character
-            if (r - l + 1) > n:
-                if s[l] != target1[l]:
-                    diff1 -= 1
-                if s[l] != target2[l]:
-                    diff2 -= 1
-                l += 1
-            
-            # Once we have a full window, track the minimum
-            if (r - l + 1) == n:
-                res = min(res, diff1, diff2)
-                
-        return res
-
-
-class Solution1:
-    def minFlips(self, s: str) -> int:
         one = zero = 0
+        hashmap = defaultdict(int)
 
         for i in range(len(s)):
            one += 1 if (i % 2 == 0 and s[i] == "1") else 0
            zero += 1 if (i % 2 == 1 and s[i] == "0") else 0 
+           hashmap[s[i]] += 1
 
-        print(zero, one)
-        if len(s) % 2 == 0:
-            return min(one+zero, len(s)-(one+zero))
+        ans = min(one+zero, len(s)-(one+zero))
+        for i in range(len(s)):
+            if s[i] == "0":
+                one = hashmap["1"] - one
+                zero = hashmap["0"] - zero - 1
+            else:
+                zero = hashmap["0"] - zero
+                one = hashmap["1"] - one + 1
+
+            ans = min(ans, min(one+zero, len(s)-(one+zero)))
+
+        return ans
 
         
