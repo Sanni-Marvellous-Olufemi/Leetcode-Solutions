b3621f6dc24599e3102e9efc718fd587f54226b5
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-03-15 10:51:27 -0500

    273 ms (20.97%) 56.5 MB (46.77%) 03-15-2026 1622-fancy-sequence Hard .py

---
+++ LeetCode/Hard/1622-fancy-sequence/1622-fancy-sequence.py
@@ -0,0 +1,32 @@
+class Fancy:
+
+    def __init__(self):
+        self.mod = 10**9 + 7
+        self.seq = []
+        self.mul = 1
+        self.add = 0
+
+    def append(self, val: int) -> None:
+        inv_mul = pow(self.mul, self.mod - 2, self.mod)
+        self.seq.append(((val - self.add) * inv_mul) % self.mod)
+
+    def addAll(self, inc: int) -> None:
+        self.add = (self.add + inc) % self.mod
+
+    def multAll(self, m: int) -> None:
+        self.mul = (self.mul * m) % self.mod
+        self.add = (self.add * m) % self.mod
+
+    def getIndex(self, idx: int) -> int:
+        if idx >= len(self.seq):
+            return -1
+            
+        return (self.seq[idx] * self.mul + self.add) % self.mod
+
+
+# Your Fancy object will be instantiated and called as such:
+# obj = Fancy()
+# obj.append(val)
+# obj.addAll(inc)
+# obj.multAll(m)
+# param_4 = obj.getIndex(idx)
