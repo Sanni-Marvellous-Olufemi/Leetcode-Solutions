4b61db189e2659ab3e6497051a656846e41854b5
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-03-18 02:11:38 -0500

    49 ms (61.68%) 55.8 MB (5.98%) 03-18-2026 2627-debounce Medium .js

---
+++ LeetCode/Medium/2627-debounce/2627-debounce.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let ref;
+
+    return function(...args) {
+        if (ref){
+            clearTimeout(ref)
+        };
+        
+        ref = setTimeout(function(){
+            return fn(...args);
+        }, t)
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
