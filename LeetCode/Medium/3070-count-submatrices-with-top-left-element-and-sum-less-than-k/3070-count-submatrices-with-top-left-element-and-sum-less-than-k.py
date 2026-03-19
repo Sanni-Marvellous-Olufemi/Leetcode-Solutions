3869c184d0579e39551fd63a296b7e5419c68603
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-03-19 17:04:46 -0500

    285 ms (55.77%) 65 MB (52.71%) 03-19-2026 3070-count-submatrices-with-top-left-element-and-sum-less-than-k Medium .py

--- LeetCode/Medium/3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ LeetCode/Medium/3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -1,6 +1,7 @@
 class Solution:
     def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
-        m, n, sets = len(grid), len(grid[0]), set()
+        m, n = len(grid), len(grid[0])
+        count = 0
 
         for r in range(m):
             for c in range(n):
@@ -8,14 +9,7 @@ class Solution:
                     grid[r][c+1] += grid[r][c]
                 if r > 0:
                     grid[r][c] += grid[r-1][c]
-
-        def walk(r, c):
-            nonlocal m, n
-
-            if (r >= m) or (c >= n) or (grid[r][c]) >  k or (r,c) in sets:
-                return 0
-
-            sets.add((r,c))
-            return walk(r+1, c) + walk(r, c+1) + 1
   
-        return walk(0, 0)
+                count += 1 if grid[r][c] <= k else 0
+
+        return count
