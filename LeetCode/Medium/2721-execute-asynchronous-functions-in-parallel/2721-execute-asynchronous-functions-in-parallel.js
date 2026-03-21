d445413f8b63c524c9d236ff35ec17b814cc93ad
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-03-20 22:48:08 -0500

    69 ms (24.91%) 54.6 MB (64.35%) 03-20-2026 2721-execute-asynchronous-functions-in-parallel Medium .js

---
+++ LeetCode/Medium/2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,35 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    const arr = functions.map((fn) => fn());
+    let count;
+    let curr = 0;
+
+    const ans = new Promise((resolve, reject) => {
+        arr.forEach((fn, i) => {
+            fn.then((val) => {
+                functions[i] = val;
+                curr += 1
+                if (curr == functions.length){
+                    resolve(functions)
+                }
+                
+            }).catch((err) => {
+                if (!count){
+                reject(err);
+                    count = 1;
+                }
+            })
+        })
+        
+    })
+    
+    return ans
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
