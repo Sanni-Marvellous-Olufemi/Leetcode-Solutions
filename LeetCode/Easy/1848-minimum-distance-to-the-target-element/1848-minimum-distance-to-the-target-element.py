dbdf1611382107e923ca65adb9f806eecb55ffff
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-04-14 12:56:31 -0500

    N/A (0%) N/A (0%) 04-14-2026 1848-minimum-distance-to-the-target-element Easy .py

---
+++ LeetCode/Easy/1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        ans = float("inf")
+        
+        for i in range(len(nums)):
+            if nums[i] == target:
+                ans = min(ans, abs(start - i))
+
+        return ans if ans != (float("inf")) else 0
+        
