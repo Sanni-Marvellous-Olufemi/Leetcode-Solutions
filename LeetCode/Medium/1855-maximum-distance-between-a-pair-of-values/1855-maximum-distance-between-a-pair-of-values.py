2d60b2a94d7266b5c17ee9bdbe64242ea53c2650
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-04-21 18:19:43 -0500

    43 ms (69.82%) 36.2 MB (9.33%) 04-21-2026 1855-maximum-distance-between-a-pair-of-values Medium .py

--- LeetCode/Medium/1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
+++ LeetCode/Medium/1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
         ans = 0
-        i, j = len(nums1)-1, len(nums2)-1
+        i, j = len(nums1) - 1, len(nums2) - 1
 
         while i >= 0 and j >= 0:
             if i >= j:
@@ -9,7 +9,7 @@ class Solution:
                 continue
 
             if nums2[j] >= nums1[i]:
-                ans = max(ans, j-i)
+                ans = max(ans, j - i)
                 i -= 1
             else:
                 j -= 1
