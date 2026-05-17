fffcc320fa9bb905efb32e0aab35a74d47f55d2e
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-05-16 21:06:47 -0400

    11 ms (65.93%) 26.5 MB (56.74%) 05-16-2026 1306-jump-game-iii Medium .py

--- LeetCode/Medium/1306-jump-game-iii/1306-jump-game-iii.py
+++ LeetCode/Medium/1306-jump-game-iii/1306-jump-game-iii.py
@@ -2,14 +2,32 @@ class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         sets = set()
 
-        def walk(i):
+        # def walk(i):
+        #     if i < 0 or i >= len(arr) or i in sets:
+        #         return False
+
+        #     if arr[i] == 0:
+        #         return True
+
+        #     sets.add(i)
+        #     return walk(i + arr[i]) or walk(i - arr[i])
+
+        # return walk(start)
+
+        queue = deque()
+        queue.append(start)
+
+        while queue:
+            i = queue.popleft()
+
             if i < 0 or i >= len(arr) or i in sets:
-                return False
+                continue
 
             if arr[i] == 0:
                 return True
 
             sets.add(i)
-            return walk(i + arr[i]) or walk(i - arr[i])
+            queue.append(i + arr[i])
+            queue.append(i - arr[i])
 
-        return walk(start)
+        return False
