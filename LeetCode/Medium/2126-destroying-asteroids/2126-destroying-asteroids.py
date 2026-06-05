78e3fcf491bbe9f81e84707e8b88b28f31441861
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-06-04 23:57:48 -0400

    76 ms (44.69%) 34 MB (55.64%) 06-04-2026 2126-destroying-asteroids Medium .py

---
+++ LeetCode/Medium/2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -0,0 +1,10 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+
+        for i in asteroids:
+            if mass < i:
+                return False
+            mass += i
+
+        return True
