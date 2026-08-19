00a06e64e30b6240ffe82cf2b6337065d1829106
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-08-18 20:12:40 -0400

    0 ms (100%) 19.5 MB (9.02%) 08-18-2026 3471-find-the-largest-almost-missing-integer Easy .py

--- LeetCode/Easy/3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
+++ LeetCode/Easy/3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
@@ -1,19 +1,20 @@
 class Solution:
     def largestInteger(self, nums: List[int], k: int) -> int:
-        sets = set()
-        hashmap = defaultdict(list)
+        hashmap = defaultdict(int)
         ans = -1
 
         if k == len(nums):
             return max(nums)
 
-        for i in range(len(nums)):
-            hashmap[nums[i]].append(i)
+        for i in nums:
+           hashmap[i] += 1
 
-        for i in hashmap:
-            arr = hashmap[i]
-
-            if (len(arr) == 1) and ((arr[0] in {0, len(nums)-1}) or k == 1):
-                ans = max(ans, i)
+        if k == 1:
+            for i in hashmap:
+                ans = max(ans, i) if hashmap[i] == 1 else ans
+            return ans
+        
+        ans = max(ans, nums[0]) if hashmap[nums[0]] == 1 else ans
+        ans = max(ans, nums[-1]) if hashmap[nums[-1]] == 1 else ans
 
         return ans
