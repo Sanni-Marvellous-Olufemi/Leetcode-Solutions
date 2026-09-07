4e72bed21790f95044921214a48defef2884a63f
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-09-06 19:18:06 -0500

    874 ms (10.49%) 232.7 MB (13.49%) 09-06-2026 0115-distinct-subsequences Hard .py

---
+++ LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        prev = 0
+        memo = {}
+
+        def walk(i, j):
+            nonlocal prev
+
+            if i >= len(s) or j >= len(t):
+                return 0
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            curr = 0
+
+            if s[i] == t[j]:
+                if j == len(t)-1:
+                    curr += 1
+
+                curr += walk(i+1, j+1)
+            
+            curr += walk(i+1, j)
+            memo[(i,j)] = curr
+            return curr
+
+        return walk(0, 0)
+            
