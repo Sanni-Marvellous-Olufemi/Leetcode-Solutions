5d3a970759cbd3d635a0ec0be553843b627b9fc1
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-09-13 01:27:24 -0500

    461 ms (27.12%) 76.5 MB (20.1%) 09-13-2026 0115-distinct-subsequences Hard .py

--- LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.py
+++ LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,5 +1,36 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
+
+        """
+            0 1 2 3 4 5 6
+
+        0   1 0 0 0 0 0 0
+        1   1 1 0 0 0 0 0
+        2   1 1 1 0 0 0 0
+        3   1 1 1 1 0 0 0
+        4   1 1 1 2 1 0 0
+        5   1 1 1 3 3 0 0
+        6   1 1 1 3 3 3 0
+        7   1 1 1 3 3 3 3
+        """
+
+        dp = [[0 for i in range(len(t)+1)] for i in range(len(s)+1)]
+
+        for i in range(len(s)+1):
+            dp[i][0] = 1
+
+        for i in range(1, len(s)+1):
+            for j in range(1, len(t)+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+
+        return dp[-1][-1]
+
+
+class Solution2:
+    def numDistinct(self, s: str, t: str) -> int:
         prev = 0
         memo = {}
 
