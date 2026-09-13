4a61621aa521b14d0efa28625baef4a114b135fe
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-09-13 01:31:43 -0500

    273 ms (64.49%) 76.7 MB (18.53%) 09-13-2026 0115-distinct-subsequences Hard .py

--- LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.py
+++ LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -20,7 +20,7 @@ class Solution:
             dp[i][0] = 1
 
         for i in range(1, len(s)+1):
-            for j in range(1, len(t)+1):
+            for j in range(1, min(i, len(t))+1):
                 if s[i-1] == t[j-1]:
                     dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
                 else:
