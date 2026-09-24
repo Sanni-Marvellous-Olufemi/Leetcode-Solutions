cdf41b13d30d9c803959ec667478dd50737b1d38
Sanni Marvellous Olufemi <msanni1@gsumail.gram.edu>
2026-09-23 23:48:00 -0500

    0 ms (100%) 19.3 MB (89.6%) 09-23-2026 0063-unique-paths-ii Medium .py

--- LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.py
+++ LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,5 +1,26 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+
+        rows = len(obstacleGrid)
+        cols = len(obstacleGrid[0])
+
+        dp = [0] * cols
+        dp[0] = 1
+
+        for row in range(rows):
+            for col in range(cols):
+                if obstacleGrid[row][col] == 1:
+                    dp[col] = 0
+
+                elif col > 0:
+                    dp[col] += dp[col - 1]
+
+        return dp[-1]
+
+
+
+class Solution1:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         row, col = len(obstacleGrid), len(obstacleGrid[0])
         memo = {}
 
